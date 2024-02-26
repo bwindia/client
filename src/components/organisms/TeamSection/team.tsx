8d6df538c0240cc273556a23b7b39dbfff0ad7b4
jellask23 <jellasaikiransatya@gmail.com>
2024-02-26 16:35:49 +0530

    fix: updated teams section

--- src/components/organisms/TeamSection/team.tsx
+++ src/components/organisms/TeamSection/team.tsx
@@ -1,165 +1,84 @@
 // Team members images
-// Leads
-import teamMember1 from 'src/assets/images/team/Sandeep.jpg'
-import teamMember2 from 'src/assets/images/team/Krishna Vamshi.jpeg'
-import teamMember3 from 'src/assets/images/team/Sharmila.jpeg'
-import teamMember4 from 'src/assets/images/team/navya.jpg'
-import teamMember5 from 'src/assets/images/team/Suksheetha.jpeg'
-import teamMember6 from 'src/assets/images/team/SatyaSai.jpg'
-import teamMember7 from 'src/assets/images/team/Trishadhi.jpeg'
+// Core team
+import coreMember1 from 'src/assets/images/team/Coreteam/Krishna.png'
+import coreMember2 from 'src/assets/images/team/Coreteam/Sandeep.png'
+import coreMember3 from 'src/assets/images/team/Coreteam/Dakshyani.png'
+import coreMember4 from 'src/assets/images/team/Coreteam/Kushal.png'
+import coreMember5 from 'src/assets/images/team/Coreteam/Jella.png'
+import coreMember6 from 'src/assets/images/team/Coreteam/Bharat.png'
 
 // Awareness Volunteers
 import member8 from 'src/assets/images/team/AwarenessVolunteers/Digital/Govardhan - Content Volunteer.png'
 import member9 from 'src/assets/images/team/AwarenessVolunteers/Digital/Mujeeb - Design Volunteer.jpg'
-import member10 from 'src/assets/images/team/AwarenessVolunteers/Digital/Nikethan - Content Volunteer.jpg'
-import member11 from 'src/assets/images/team/AwarenessVolunteers/Digital/Riddhi - Content Volunteer.jpeg'
-import member12 from 'src/assets/images/team/AwarenessVolunteers/Digital/Sumitra - Content Volunteer.jpeg'
-import member13 from 'src/assets/images/team/AwarenessVolunteers/Digital/Vishal - Content Strategist.jpg'
+import member10 from 'src/assets/images/team/BloodBridgeVolunteers/Bhavika.png'
 import member14 from 'src/assets/images/team/AwarenessVolunteers/On ground/Datta Sai.jpg'
-import member15 from 'src/assets/images/team/AwarenessVolunteers/On ground/Kushal - Awareness Volunteer.jpg'
 import member16 from 'src/assets/images/team/AwarenessVolunteers/On ground/picture - Pallavi Madabhushi.jpeg'
 import member17 from 'src/assets/images/team/AwarenessVolunteers/On ground/Shahela - Awareness Volunteer (1).png'
+import teamMember7 from 'src/assets/images/team/Trishadhi.jpeg'
 
 // Blood Bridge Volunteers
 import member18 from 'src/assets/images/team/BloodBridgeVolunteers/Deepti.jpeg'
-import member19 from 'src/assets/images/team/BloodBridgeVolunteers/Hemanth.jpeg'
 import member20 from 'src/assets/images/team/BloodBridgeVolunteers/Krishna Sai.png'
-import member21 from 'src/assets/images/team/BloodBridgeVolunteers/Manoj.jpeg'
-import member22 from 'src/assets/images/team/BloodBridgeVolunteers/Pradeep.jpeg'
-import member23 from 'src/assets/images/team/BloodBridgeVolunteers/Priyanka.jpg'
 import member24 from 'src/assets/images/team/BloodBridgeVolunteers/Sai Sukesh.jpg'
 import member25 from 'src/assets/images/team/BloodBridgeVolunteers/Shanth_Photo - Shanth Kumar Goud.jpeg'
 import member26 from 'src/assets/images/team/BloodBridgeVolunteers/Smithi.png'
-import member27 from 'src/assets/images/team/BloodBridgeVolunteers/Sofia.jpeg'
-import member28 from 'src/assets/images/team/BloodBridgeVolunteers/Sushma.jpg'
+import member15 from 'src/assets/images/team/BloodBridgeVolunteers/Srihitha.jpg'
+import teamMember3 from 'src/assets/images/team/BloodBridgeVolunteers/Sharmila.jpeg'
+import teamMember4 from 'src/assets/images/team/BloodBridgeVolunteers/navya.jpg'
+import teamMember6 from 'src/assets/images/team/SatyaSai.jpg'
 
 import { ITeamMember } from 'src/utils/types'
 
 // Team members
 export const TEAM_MEMBERS: ITeamMember[] = [
   {
-    image: teamMember1,
-    name: 'Sandeep Kavety',
-    role: 'Co-Founder & Technology Head'
+    image: coreMember1,
+    name: 'Krishna Vamshi',
+    role: 'Co-Founder & CEO'
   },
   {
-    image: teamMember2,
-    name: 'Krishna Vamshi',
-    role: 'Co-Founder & Strategy Head'
+    image: coreMember2,
+    name: 'Sandeep Kavety',
+    role: 'Co-Founder, Blood Bridge & Operations'
+  },
+  {
+    image: coreMember3,
+    name: 'Dakshayani Ravi',
+    role: 'Strategic Partnerships'
+  },
+  {
+    image: coreMember4,
+    name: 'Kushal Mor',
+    role: 'Awareness'
+  },
+  {
+    image: coreMember5,
+    name: 'Jella SaiKiran',
+    role: 'Technology'
+  },
+  {
+    image: coreMember6,
+    name: 'Bharat Devana',
+    role: 'Digital Campaigns & Communication'
   },
   {
     image: teamMember3,
     name: 'Sharmila',
-    role: 'Blood Bridge Lead'
+    role: 'Blood Bridge Volunteer'
   },
   {
     image: teamMember4,
     name: 'Navya Neela',
-    role: 'Blood Bridge Lead'
-  },
-  {
-    image: teamMember5,
-    name: 'Suksheetha',
-    role: 'Design Lead'
-  },
-  {
-    image: teamMember6,
-    name: 'Satya Sai',
-    role: 'Human Capital Lead'
-  },
-  {
-    image: teamMember7,
-    name: 'Trishadhi',
-    role: 'Content Lead'
-  },
-  {
-    image: member8,
-    name: 'Govardhan',
-    role: 'Content Volunteer'
-  },
-  {
-    image: member9,
-    name: 'Mujeeb',
-    role: 'Design Volunteer'
-  },
-  {
-    image: member10,
-    name: 'Nikethan',
-    role: 'Content Volunteer'
-  },
-  {
-    image: member11,
-    name: 'Riddhi',
-    role: 'Content Volunteer'
-  },
-  {
-    image: member12,
-    name: 'Sumitra',
-    role: 'Content Volunteer'
-  },
-  {
-    image: member13,
-    name: 'Vishal',
-    role: 'Content Volunteer'
-  },
-  {
-    image: member14,
-    name: 'Datta Sai',
-    role: 'Awareness Volunteer'
-  },
-  {
-    image: member15,
-    name: 'Kushal',
-    role: 'Awareness Volunteer'
+    role: 'Blood Bridge Volunteer'
   },
   {
     image: member16,
     name: 'Pallavi Madabhushi',
-    role: 'Awareness Volunteer'
-  },
-  {
-    image: member17,
-    name: 'Shahela',
-    role: 'Awareness Volunteer'
-  },
-  {
-    image: member18,
-    name: 'Deepti',
     role: 'Blood Bridge Volunteer'
   },
   {
-    image: member19,
-    name: 'Hemanth',
-    role: 'Blood Bridge Volunteer'
-  },
-  {
-    image: member20,
-    name: 'Krishna Sai',
-    role: 'Blood Bridge Volunteer'
-  },
-  {
-    image: member21,
-    name: 'Manoj',
-    role: 'Blood Bridge Volunteer'
-  },
-  {
-    image: member22,
-    name: 'Pradeep',
-    role: 'Blood Bridge Volunteer'
-  },
-  {
-    image: member23,
-    name: 'Priyanka',
-    role: 'Blood Bridge Volunteer'
-  },
-  {
-    image: member24,
-    name: 'Sai Sukesh',
-    role: 'Blood Bridge Volunteer'
-  },
-  {
-    image: member25,
-    name: 'Shanth Kumar Goud',
+    image: member15,
+    name: 'Srihitha',
     role: 'Blood Bridge Volunteer'
   },
   {
@@ -168,13 +87,58 @@ export const TEAM_MEMBERS: ITeamMember[] = [
     role: 'Blood Bridge Volunteer'
   },
   {
-    image: member27,
-    name: 'Sofia',
+    image: member24,
+    name: 'Sai Sukesh',
     role: 'Blood Bridge Volunteer'
   },
   {
-    image: member28,
-    name: 'Sushma',
+    image: member18,
+    name: 'Deepti',
     role: 'Blood Bridge Volunteer'
+  },
+  {
+    image: member20,
+    name: 'Krishna Sai',
+    role: 'Blood Bridge Volunteer'
+  },
+  {
+    image: member25,
+    name: 'Shanth Kumar Goud',
+    role: 'Blood Bridge Volunteer'
+  },
+  {
+    image: teamMember6,
+    name: 'Satya Sai',
+    role: 'Bridge Manager'
+  },
+  {
+    image: teamMember7,
+    name: 'Trishadhi',
+    role: 'Digital Communications Volunteer'
+  },
+  {
+    image: member9,
+    name: 'Mujeeb',
+    role: 'Digital Communications Volunteer'
+  },
+  {
+    image: member10,
+    name: 'Bhavika',
+    role: 'Digital Communications Volunteer'
+  },
+  {
+    image: member14,
+    name: 'Datta Sai',
+    role: 'Awareness Volunteer'
+  },
+  {
+    image: member17,
+    name: 'Shahela',
+    role: 'Awareness Volunteer'
+  },
+  {
+    image: member8,
+    name: 'Govardhan',
+    role: 'Awareness Volunteer'
   }
 ]
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,10 +3,40 @@ const Dotenv = require('dotenv-webpack')
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  target: 'web',
+  output: {
+    filename: '[name].[chunkhash].js'
+  },
   plugins: [
     new Dotenv({
       path: 'environments/.prod.env'
     })
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    mergeDuplicateChunks: true,
+    removeEmptyChunks: true,
+    sideEffects: false,
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          enforce: true,
+          name: (module) => {
+            const [, match] = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]([^\\/]*)([\\/]([^\\/]*))?([\\/]([^\\/]*))?|$)/
+            )
+
+            return `vendors/${match.replace('@', '')}`
+          }
+        }
+      }
+    }
+  },
+
+  performance: {
+    maxEntrypointSize: Infinity,
+    maxAssetSize: 1024 ** 2
+  }
 }
